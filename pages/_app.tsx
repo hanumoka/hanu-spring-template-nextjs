9adc7de4d923af32dfcca0fcd37e5f8a49d48f68
hanumoka <amagramer@gmail.com>
2022-08-11 22:31:20 +0900

    tailwind 다크테마 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { AppProps } from 'next/app'
-
+import {AppProps} from 'next/app'
+import {ThemeProvider} from 'next-themes';
 import '../styles/global.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <ThemeProvider enableSystem={true} attribute="class">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
 export default MyApp;
